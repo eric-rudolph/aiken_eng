7ce9583eea72926a4320fb80d1d24451eb67c379
eric-rudolph <eric.rudolph@aiken-engineering.com>
2026-01-08 15:40:48 -0600

    added some plotting functions, unit module, bump version to 0.3.0

--- src/aiken_eng/ansys.py
+++ src/aiken_eng/ansys.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-def read_nodes(filename: str|Path) -> dict:
+
+def read_nodes(filename: str | Path) -> dict:
     """
     Reads the NLIST file from ANSYS APDL and returns a dictionary of the nodes.  This can be useful to create a
     pandas.Dataframe.
@@ -31,6 +32,7 @@ def read_nodes(filename: str|Path) -> dict:
 
     return nodes
 
+
 def read_prnsol(filename: str | Path) -> dict:
     """
     Reads the PRNSOL output and returns a dict {NODE, KEY} where KEY is
@@ -39,7 +41,7 @@ def read_prnsol(filename: str | Path) -> dict:
     path = Path(filename)
     nodes = {}
 
-    with open(path) as f:
+    with path.open(mode="r", encoding="utf-8") as f:
         content = f.readlines()
 
     node = []
@@ -59,6 +61,7 @@ def read_prnsol(filename: str | Path) -> dict:
 
     return nodes
 
+
 def read_pretab(filename: str | Path) -> dict:
     """
     Reads the PRETAB output and returns a dict {ELEMENT, KEY} where KEY is
@@ -67,7 +70,7 @@ def read_pretab(filename: str | Path) -> dict:
     path = Path(filename)
     elements = {}
 
-    with open(path) as f:
+    with path.open(mode="r", encoding="utf-8") as f:
         content = f.readlines()
 
     for line in content:
@@ -84,3 +87,102 @@ def read_pretab(filename: str | Path) -> dict:
                         elements[key].append(float(line.split()[i]))
 
     return elements
+
+
+def read_elements(filename: str | Path) -> dict:
+    """Returns a dict {ELEM, NODE1, NODE2} for beam elements."""
+    path = Path(filename)
+    elements = {}
+
+    with path.open(mode="r", encoding="utf-8") as f:
+        content = f.readlines()
+
+    elem = []
+    node_1 = []
+    node_2 = []
+
+    for line in content:
+        if len(line.split()) > 0:
+            if (line.split()[0]).isnumeric():
+                elem.append(int(line.split()[0]))
+                node_1.append(int(line.split()[6]))
+                node_2.append(int(line.split()[7]))
+
+    elements["ELEM"] = elem
+    elements["NODE1"] = node_1
+    elements["NODE2"] = node_2
+
+    return elements
+
+def read_linearized_stress(filename: str) -> dict:
+    """This file reads the linearized stress output from ANSYS and returns a dict."""
+    path = Path(filename)
+    results = {}
+    with path.open(mode="r", encoding="utf-8") as f:
+        content = f.readlines()
+
+    results["path"] = content[1].split()[9]
+
+    if content[3].split()[0] == "RADIUS":
+        addlines = 0
+        results["radius"] = float(content[3].split()[4])
+        results["inside node"] = int(content[6].split()[3])
+        results["outside node"] = int(content[6].split()[7])
+
+    else:
+        addlines = -3
+        results["inside node"] = int(content[4].split()[3])
+        results["outside node"] = int(content[4].split()[7])
+
+    results["SXm"] = float(content[16 + addlines].split()[0])
+    results["SYm"] = float(content[16 + addlines].split()[1])
+    results["SZm"] = float(content[16 + addlines].split()[2])
+    results["SXYm"] = float(content[16 + addlines].split()[3])
+    results["SYZm"] = float(content[16 + addlines].split()[4])
+    results["SXZm"] = float(content[16 + addlines].split()[5])
+    results["S1m"] = float(content[18 + addlines].split()[0])
+    results["S2m"] = float(content[18 + addlines].split()[1])
+    results["S3m"] = float(content[18 + addlines].split()[2])
+    results["SINTm"] = float(content[18 + addlines].split()[3])
+    results["SEQVm"] = float(content[18 + addlines].split()[4])
+    results["SXb inside"] = float(content[22 + addlines].split()[1])
+    results["SXb outside"] = float(content[24 + addlines].split()[1])
+    results["SYb inside"] = float(content[22 + addlines].split()[2])
+    results["SYb outside"] = float(content[24 + addlines].split()[2])
+    results["SZb inside"] = float(content[22 + addlines].split()[3])
+    results["SZb outside"] = float(content[24 + addlines].split()[3])
+    results["SXYb inside"] = float(content[22 + addlines].split()[4])
+    results["SXYb outside"] = float(content[24 + addlines].split()[4])
+    results["SYZb inside"] = float(content[22 + addlines].split()[5])
+    results["SYZb outside"] = float(content[24 + addlines].split()[5])
+    results["S1b inside"] = float(content[26 + addlines].split()[1])
+    results["S1b outside"] = float(content[28 + addlines].split()[1])
+    results["S2b inside"] = float(content[26 + addlines].split()[2])
+    results["S2b outside"] = float(content[28 + addlines].split()[2])
+    results["S3b inside"] = float(content[26 + addlines].split()[3])
+    results["S3b outside"] = float(content[28 + addlines].split()[3])
+    results["SINTb inside"] = float(content[26 + addlines].split()[4])
+    results["SINTb outside"] = float(content[28 + addlines].split()[4])
+    results["SEQVb inside"] = float(content[26 + addlines].split()[5])
+    results["SEQVb outside"] = float(content[28 + addlines].split()[5])
+    results["SXp inside"] = float(content[42 + addlines].split()[1])
+    results["SXp outside"] = float(content[44 + addlines].split()[1])
+    results["SYp inside"] = float(content[42 + addlines].split()[2])
+    results["SYp outside"] = float(content[44 + addlines].split()[2])
+    results["SZp inside"] = float(content[42 + addlines].split()[3])
+    results["SZp outside"] = float(content[44 + addlines].split()[3])
+    results["SXYp inside"] = float(content[42 + addlines].split()[4])
+    results["SXYp outside"] = float(content[44 + addlines].split()[4])
+    results["SYZp inside"] = float(content[42 + addlines].split()[5])
+    results["SYZp outside"] = float(content[44 + addlines].split()[5])
+    results["S1p inside"] = float(content[46 + addlines].split()[1])
+    results["S1p outside"] = float(content[48 + addlines].split()[1])
+    results["S2p inside"] = float(content[46 + addlines].split()[2])
+    results["S2p outside"] = float(content[48 + addlines].split()[2])
+    results["S3p inside"] = float(content[46 + addlines].split()[3])
+    results["S3p outside"] = float(content[48 + addlines].split()[3])
+    results["SINTp inside"] = float(content[46 + addlines].split()[4])
+    results["SINTp outside"] = float(content[48 + addlines].split()[4])
+    results["SEQVp inside"] = float(content[46 + addlines].split()[5])
+    results["SEQVp outside"] = float(content[48 + addlines].split()[5])
+    return results
